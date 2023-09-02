65a891d38d902fe0a7ad5195a4e96ec24098c3a7
Samar-Alhariqi <75090108+Samar-Alhariqi@users.noreply.github.com>
2023-09-02 05:01:45 +0300

    Add files via upload

---
+++ dht.php
@@ -0,0 +1,28 @@
+<?php
+$hostname = "localhost";
+$username = "root";
+$password = "";
+$database = "data";
+
+$conn = mysqli_connect($hostname, $username, $password, $database);
+
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
+}
+
+echo "Database connection is OK";
+
+if(isset($_POST["temperature"]) && isset($_POST["humidity"])) {
+  $t = $_POST["temperature"];
+  $h = $_POST["humidity"];
+
+  $sql = "INSERT INTO dht (temperature, humidity) VALUSE (".$t.", ".$h.")";
+
+  if (mysqli_query($conn, $sql)) {
+    echo "\nNew record created successfully";
+  } else {
+     echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+  }
+}
+
+?>
